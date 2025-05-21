eb5676cd53b0b10f4e2db755f6439a56bed906bc
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-05-21 21:15:21 +0200

    Home_2

--- immacolata/src/components/Home.jsx
+++ immacolata/src/components/Home.jsx
@@ -7,16 +7,26 @@ const Home = () => {
   const [scrolled, setScrolled] = useState(false);
   const [scrollY, setScrollY] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY)
-      const offset = window.scrollY;
-      setScrolled(offset > 100);
-    };
+  const limitedScroll = Math.min(scrollY, 200); // valore massimo = 300
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  useEffect(() => {
+  let ticking = false;
+
+  const handleScroll = () => {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        setScrollY(window.scrollY);
+        setScrolled(window.scrollY > 100);
+        ticking = false;
+      });
+      ticking = true;
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
+
 
   return (
     <div className="home">
@@ -32,19 +42,42 @@ const Home = () => {
         </nav>
       </header>
 
-      <div className={`image ${scrolled ? 'shrink' : ''}`}>
+      <div className="image-container" style={{ transform: `translateY(${scrollY * 0.5}px)` }}>
+      <div className={`image-inner ${scrolled ? 'shrink' : ''}`}>
         <img src="/images/ImmacolataBgHome.png" className="foto" alt="Copertina" />
         <div
             className="overlay"
             style={{
-            opacity: Math.min(0.3, scrollY / 200)  // aumenta l'opacità fino a 1
-        }}
+              opacity: 0.5-Math.min(scrollY / 300, 0.3)  // aumenta l'opacità fino a 1
+            }}
         />
+        <div
+          className='benvenuto'
+          style={{
+            transform: `translate(-50%, calc(-50% + ${limitedScroll * 0.2}px))`,
+          }}
+        >
+          Benvenuto
+        </div>
+        <div
+          className='invito'
+          style={{
+            transform: `translate(-50%, calc(-50% + ${limitedScroll * 0.2}px))`,
+          }}
+        >
+          Scopri la Parrocchia
+        </div>
       </div>
-
+      </div>
       <div className={`content ${scrolled ? 'visible' : ''}`} id="content">
-        <h2>Benvenuto!</h2>
-        <p>Questa sezione appare man mano che scrolli.</p>
+        <h2>Parrocchia Immacolata di Gioia del colle</h2>
+        <div className='orari'>
+          <h>Orari Sante messe</h>
+          <ul>Dal lunedì al sabato: 18.30</ul>
+          <ul>La domenica:</ul>
+            <ul>mattina: 10.30</ul>
+            <ul>pomeriggio: 18.30</ul>
+        </div>
       </div>
     </div>
   );
